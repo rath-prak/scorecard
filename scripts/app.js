764265d99efa5c1d309578cb900bd4c7dd82e967
rath-prak <rathanap@gmail.com>
2017-03-09 02:15:47 +1300

    refactor compoments

--- scripts/app.js
+++ scripts/app.js
@@ -1,59 +1,83 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// import Players from './components/Players'
 
-class Application extends React.Component{
-  render (){
-    return (
-      <div className="scoreboard">
-        <header className="header">
-          <h1>Game Scorecard</h1>
-        </header>
+// header
 
-        <div className="players">
-          <div className="player">
-            <div className="player-name">Harvestor Of Sorrow</div>
-            <div className="player-score">
-              <div className="counter">
-                <buttom className="counter-action decrement"> - </buttom>
-                <div className="counter-score">56</div>
-                <buttom className="counter-action increment"> + </buttom>
-              </div>
-            </div>
-          </div>
-        </div>
+function Header(props){
+  return (
+    <header className="header">
+      <h1>{props.title}</h1>
+    </header>
+  )
+}
 
-        <div className="players">
-          <div className="player">
-            <div className="player-name">Wolfman</div>
-            <div className="player-score">
-              <div className="counter">
-                <buttom className="counter-action decrement"> - </buttom>
-                <div className="counter-score">52</div>
-                <buttom className="counter-action increment"> + </buttom>
-              </div>
-            </div>
-          </div>
-        </div>
+Header.propTypes = {
+  title: React.PropTypes.string.isRequired
+}
 
-        <div className="players">
-          <div className="player">
-            <div className="player-name">Butter Bean</div>
-            <div className="player-score">
-              <div className="counter">
-                <buttom className="counter-action decrement"> - </buttom>
-                <div className="counter-score">46</div>
-                <buttom className="counter-action increment"> + </buttom>
-              </div>
-            </div>
-          </div>
-        </div>
+Header.defaultProps = {
+  title: "Scorecard 2017"
+}
 
-      {/* end of main application */}
+// Counter
+function Counter(props){
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <div className="counter-score">{props.score}</div>
+      <button className="counter-action increment"> + </button>
+    </div>
+  )
+}
+
+Counter.propTypes = {
+  score: React.PropTypes.number.isRequired,
+}
+
+// player
+
+function Player(props){
+  return (
+    <div className="player">
+      <div className="player-name">{props.name}</div>
+      <div className="player-score">
+        <Counter score={props.score}/>
       </div>
-    )
-  }
+    </div>
+  )
+}
+
+Player.propTypes = {
+  name: React.PropTypes.string.isRequired,
+  score: React.PropTypes.number.isRequired,
+}
+
+
+function Application (props){
+  return (
+    <div className="scoreboard">
+
+      <Header title={props.title}/>
+
+      <div className="players">
+        <Player name="Harvestor Of Sorrow" score={32} />
+        <Player name="Creeping Death" score={42} />
+        <Player name="Master Of Puppets" score={22} />
+      </div>
+
+
+    {/* end of main application */}
+    </div>
+  )
+};
+
+Application.propTypes = {
+  title: React.PropTypes.string
+}
+
+Application.defaultProps = {
+  title: "Scorecard 2017..."
 }
 
 
